b552f522f93a4b88f9eb60344d348f898f3ab290
cicekabdulla <chichek.abdullayeva@gmail.com>
2022-08-23 15:19:10 +0300

    File updated

--- DevelopersProject/src/js/modules/form.js
+++ DevelopersProject/src/js/modules/form.js
@@ -1,13 +1,18 @@
 'use strict'
 
+import http from '../services/api.js';
+
 function form() {
     const form = document.querySelector('form');
+    const { request } = http();
 
     form.addEventListener('submit', (e) => {
         e.preventDefault();
 
+        const id = new Date().getTime().toString().slice(7, 14);
         const data = Object.fromEntries(new FormData(e.target));
-        console.log(data);
+        data.id = id;
+        request('http://localhost:3000/callRequest', 'POST', data)
     })
 }
 
