7ec47c04eaa188893b737410b6bb02514b19559f
cicekabdulla <chichek.abdullayeva@gmail.com>
2022-08-24 19:54:40 +0300

    File updated

--- FoodProject/js/modules/calc.js
+++ FoodProject/js/modules/calc.js
@@ -3,7 +3,7 @@
 function calc() {
     const result = document.querySelector('.calculating__result span');
 
-    let height, weight, age, gender, ratio;
+    let height, weight, age, gender = 'female', ratio = 1.2;
 
     function getCalc() {
         if (!height || !weight || !age || !ratio) {
@@ -12,9 +12,9 @@ function calc() {
         }
 
         if (gender === 'male') {
-            result.textContent = 88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age);
+            result.textContent = ((88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio).toFixed(0);
         } else {
-            result.textContent = 447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)
+            result.textContent = ((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio).toFixed(0);
         }
     }
 
@@ -38,11 +38,38 @@ function calc() {
                 parent.forEach(child => child.classList.remove(activeClass));
                 target.classList.add(activeClass)
                 console.log(gender, ratio);
-            });
 
-        });
+                getCalc()
+            })
+
+        })
     }
 
+    function getDynamicInformation(selector) {
+        const parent = document.querySelectorAll(`${selector} input`);
+
+        parent.forEach(child => {
+            child.addEventListener('input', (e) => {
+                const target = e.target;
+
+                switch (target.getAttribute('id')) {
+                    case 'weight':
+                        weight = e.target.value;
+                        break;
+                    case 'height':
+                        height = target.value;
+                        break;
+                    case 'age':
+                        age = target.value;
+                        break;
+                }
+
+                getCalc()
+            })
+        })
+    }
+
+    getDynamicInformation('.calculating__choose_medium');
     getStaticInformation('#gender', 'calculating__choose-item_active');
     getStaticInformation('.calculating__choose_big', 'calculating__choose-item_active');
 }
